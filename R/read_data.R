d4678b4b11e8f41bf8ed6f5a41ea472b53ceb649
Jinane Jouni <jinane.jouni@hotmail.com>
2025-11-17 10:57:12 +0200

    added message when done reading

--- R/read_data.R
+++ R/read_data.R
@@ -103,8 +103,8 @@ read_data <- function(indices = NA,
     results[[j]] <- microdata
   }
 
-  if( j==1) {message("Done Reading: ", j, "file")}
-   else {message("Done Reading: ", j, "files")}
+  if( j==1) {message("Done Reading: ", j, " file")}
+   else {message("Done Reading: ", j, " files")}
 
   return(results)
 }
