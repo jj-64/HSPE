2d1bd81d161955874dd87c39d208af6507a56b5c
Jinane Jouni <jinane.jouni@hotmail.com>
2025-11-18 15:40:05 +0200

    intial commit

--- Execution/Layer III - MicroData.R
+++ Execution/Layer III - MicroData.R
@@ -27,10 +27,13 @@ summarize_data <- function(indices = NA,
       mlfit.gb2(fyy2)
       fit_FISK <- fitdistrplus::fitdist(as.numeric(fyy2), "llogis", method = "mle", control = list(trace = 1))
 
-      fit_LN <- fitdistr(fyy2, densfun = "lognormal")
+      fit_LN <- fitdistr(y, densfun = "lognormal")
+      fit_LN <- fitdistrplus::fitdist(y, distr = "lnorm")
+      fit_GB2 <- fitdistrplus::fitdist(y, distr = "gb2")
+      fit_GB2 <- fitdistrplus::fitdist(y, distr = "dagum")
 
-      fit_NP <- fitdist(as.numeric(fyy2), "NPareto",start=list(alpha=0.1,Beta=min(fyy2)),
-                        method="mge",control = list(trace = 1))
+      #fit_NP <- fitdist(as.numeric(fyy2), "NPareto",start=list(alpha=0.1,Beta=min(fyy2)),
+                        #method="mge",control = list(trace = 1))
     }
   }
 }
