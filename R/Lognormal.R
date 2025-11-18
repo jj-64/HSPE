e7acb765ca30bd9959a18f5b1104fdd504dd93a2
Jinane Jouni <jinane.jouni@hotmail.com>
2025-11-18 15:40:58 +0200

    optimized code

--- R/Lognormal.R
+++ R/Lognormal.R
@@ -1,41 +1,95 @@
 
 # Lognormal sigma from Gini --------------
-Sigma_LN<- function(Gini) {
+#' Lognormal Sigma From Gini
+#'
+#' Computes the lognormal sigma parameter from the Gini coefficient using:
+#'   sigma = sqrt(2) * qnorm((1 + Gini)/2)
+#'
+#' @param Gini Numeric in (0,1). The Gini coefficient.
+#'
+#' @return The lognormal sigma.
+#' @export
+#' @examples
+#' Sigma_LN(0.36)
+#' [1] 0.661426
+Sigma_LN <- function(Gini) {
   sqrt(2) * qnorm((1 + Gini) / 2)
 }
 
+
 ## Lognormal meanlog (mu) -------------
-mu_LN = function(mean_y, LN_sigma){
-  log(mean_y) - LN_sigma^2/2
+#' Lognormal Meanlog (mu)
+#'
+#' For a lognormal distribution:
+#'   mean = exp(mu + sigma^2 / 2)
+#' so
+#'   mu = log(mean) - sigma^2/2
+#'
+#' @param mean_y numerical value, observed mean of the variable
+#' @param sigma numerical value, lognormal scale positive parameter
+#'
+#' @return mu (meanlog)
+#' @export
+#' @examples
+#' mu_LN(1000, 0.36)
+#' [1] 6.8429
+mu_LN <- function(mean_y, sigma) {
+  log(mean_y) - sigma^2 / 2
 }
 
+
 ## Standard Error for Sigma parameter ------------
-se_sigma_LN = function(mean_y, Gini, se_Gini){
-
-  #LN_sigma = Sigma_LN(Gini)
-  #mu_LN    <- mu_LN(mean_y, LN_sigma)
-
-  # Derivative dσ/dG
-  z <- (1 + Gini) / 2
-  d_sigma_dG <- sqrt(2) * (1/dnorm(qnorm(z))) * (1/2)
+#' Standard Error of Lognormal Sigma From Gini
+#'
+#' Uses delta method: SE(sigma) = |d sigma / d G| * SE(Gini)
+#'
+#' @param mean_y numerical value, observed mean of the variable. Not used (kept for consistency).
+#' @param Gini Numeric in (0,1). The Gini coefficient.
+#' @param se_Gini Standard error of the Gini index.
+#'
+#' @return positive value, Standard error of sigma
+#' @export
+#' @examples
+#' se_sigma_LN(1000,0.36, 0.01)
+#' [1] 0.01977
+se_sigma_LN <- function(mean_y, Gini, se_Gini) {
+  if( Gini <=0 | Gini >=1){stop("Gini should be between 0 and 1")}
+  if (mean_y <=0) stop ("mean_y should be positive")
+   z <- (1 + Gini) / 2
+  d_sigma_dG <- sqrt(2) * (1 / dnorm(qnorm(z))) * 0.5
 
   se_sigma_LN <- abs(d_sigma_dG) * se_Gini
   return(se_sigma_LN)
 }
 
 ## Standard error for mu parameter -----------------
-se_mu_LN = function(mean_y, Gini, se_mean, se_Gini){
+#' Standard Error of Lognormal mu
+#'
+#' Uses delta method based on partial derivatives wrt mean and Gini.
+#'
+#' @param mean_y numerical value, Mean of the variable.
+#' @param Gini Numeric in (0,1). The Gini coefficient.
+#' @param se_mean standard error of the mean
+#' @param se_Gini standard error of the Gini index
+#'
+#' @return positive value, standard error of mu
+#' @export
+#' @examples
+#' se_mu_LN(1000,0.36, 250, 0.01)
+#' [1] 0.2503419
+se_mu_LN <- function(mean_y, Gini, se_mean, se_Gini) {
+  if( Gini <=0 | Gini >=1){stop("Gini should be between 0 and 1")}
+  if (mean_y <=0) stop ("mean_y should be positive")
+  LN_sigma <- Sigma_LN(Gini)
 
-  LN_sigma = Sigma_LN(Gini)
-
-  # Derivative dμ/dmean
+  # d mu / d mean
   d_mu_dmean <- 1 / mean_y
 
-  # Derivative dσ/dG
+  # d sigma / d G
   z <- (1 + Gini) / 2
-  d_sigma_dG <- sqrt(2) * (1 / dnorm(qnorm(z))) * (1/2)
+  d_sigma_dG <- sqrt(2) * (1 / dnorm(qnorm(z))) * 0.5
 
-  # Derivative dμ/dG = -sigma * dσ/dG
+  # d mu / d G
   d_mu_dG <- -LN_sigma * d_sigma_dG
 
   se_mu_LN <- sqrt(
@@ -46,47 +100,88 @@ se_mu_LN = function(mean_y, Gini, se_mean, se_Gini){
 }
 
 ## Compute the Lorenz Curve - Lopez ----------------
-Lorenz_LN = function(LN_sigma, P) {
+#' Lognormal Lorenz Curve
+#'
+#' Computes L(p) = Φ(Φ⁻¹(p) − sigma)
+#'
+#' @param P Quantiles in [0,1]
+#' @param sigma numerical value, lognormal scale positive parameter
+#'
+#' @return Lorenz curve values.
+#' @export
+#' @examples
+#' Lorenz_LN(Gini = 0.36)
+#' # [1] 0.02600944 0.06641343 0.11784541 0.18015538 0.25416958 0.34160788 0.44550533 0.57150036
+#' # [9] 0.73241244 1.00000000
+Lorenz_LN <- function(p = seq(0.1,1,0.1), Gini) {
+  if(Gini <=0 | Gini >=1) stop("sigma should be between 0 and 1")
+  sigma = Sigma_LN(Gini)
 
-  Lorenz=0
-  for(i in 1:length(P)) {
-    Lorenz[i] = pnorm(qnorm(P[i]) - LN_sigma)
-  }
-  return(Lorenz)
+  pnorm(qnorm(p) - sigma)
 }
+
 
 ## PDF of Lognormal, same as dlnorm -----------
-pdf_LN = function(y,mu_LN,LN_sigma) { ## lmu is mean in logscale
-  exp(-(log(y) - mu_LN )^2/( 2*LN_sigma^2) ) / (y*LN_sigma*sqrt(2*pi))
+#' Lognormal PDF (fast)
+#'
+#' @param y numerical vector of the variable
+#' @param mu numerical value, lognormal location parameter
+#' @param sigma numerical value, lognormal scale positive parameter
+#'
+#' @return Density, probability between 0 and 1
+#' @export
+#' @examples
+#' pdf_LN(100, 6, 12)
+#' [1] 0.0003302136
+#' pdf_LN(c(100, 200), 6, 12)
+#' [1] 0.0003302136 0.0001659420
+pdf_LN <- function(y, mu, sigma) {
+  exp(-(log(y) - mu)^2 / (2 * sigma^2)) / (y * sigma * sqrt(2*pi))
 }
 
+
 ## CDF of Lognormal, same as plnorm ------------
-CDF_LN = function(y, mean_y, LN_sigma){
-  #pnorm((log(y) - mu_LN)/LN_sigma)
-  pnorm( (log(y / mean_y) / LN_sigma) + LN_sigma / 2 )
+#' Lognormal CDF Expressed Using (mean, sigma)
+#'
+#' @param y numerical vector for the mean income
+#' @param mean_y numerical value, average income
+#' @param sigma numerical value, lognormal scale positive parameter
+#'
+#' @return CDF, probability between 0 and 1
+#' @export
+#' @examples
+#' CDF_LN(1500, mean_y = 6000, 0.6)
+#' [1] 0.02218964
+#' CDF_LN(c(1500,7500), mean_y = 6000, 0.6)
+#' [1] 0.02218964 0.74917820
+CDF_LN <- function(y, mean_y, sigma) {
+  if(sigma <=0) stop("sigma should be positive")
+  if(mean_y <=0) stop("mean_y should be positive")
+  pnorm((log(y / mean_y) / sigma) + sigma/2)
 }
 
 ## Function for the Lorenz Curve for the country --------------
-Lorenz_LN = function(y, mu_LN, LN_sigma) { ## the income group y, mean income and sigma
-
+Lorenz_LN_fromIncome = function(y, mu, sigma) { ## the income group y, mean income and sigma
+  if(sigma <=0) stop("sigma should be positive")
+  if(mean_y <=0) stop("mean_y should be positive")
   ## vector for the intergrals
 
-  integrand <- function(z) {z * dlnorm(z, mu_LN, LN_sigma) }
+  integrand <- function(z) {z * dlnorm(z, mu, sigma) }
   int = integrate(integrand, lower = 0, upper = y)$value
 
   ## Formula
-  Lorenz = exp(-(mu_LN + LN_sigma^2/2)) * int
+  Lorenz = exp(-(mu + sigma^2/2)) * int
   return(Lorenz)
 }
 
 ## Function for the Lorenz Curve for the region ---------------
-R_Lorenz_LN = function(y, R_mu, popw, mu_LN, sigma) { ## the income group y, regional mean income and vector of population weight, vector of mu_LN, of sigma
+R_Lorenz_LN = function(y, R_mu, popw, mu, sigma) { ## the income group y, regional mean income and vector of population weight, vector of mu, of sigma
 
   ## vector for the intergrals
   int = 0
 
-  for( i in 1:length(mu_LN)) {
-    integrand <- function(z) {z * dlnorm(z, mu_LN[i], sigma[i]) }
+  for( i in 1:length(mu)) {
+    integrand <- function(z) {z * dlnorm(z, mu[i], sigma[i]) }
     int[i] = integrate(integrand, lower = 0, upper = y)$value
   }
 
@@ -96,28 +191,31 @@ R_Lorenz_LN = function(y, R_mu, popw, mu_LN, sigma) { ## the income group y, reg
 }
 
 ## Headcount Standard Error ---------------
-HC_se_LN <- function(p, mean_y, LN_sigma, se_mean, se_sigma, se_mu, cov_mu_sigma = 0) {
+#' Standard Error of Lognormal Headcount Ratio
+#'
+#' @param p Poverty line
+#' @param mean_y numerical value, observed mean of the variable
+#' @param sigma numerical value, lognormal scale positive parameter
+#' @param se_mean standard error of the mean
+#' @param se_sigma standard error of sigma parameter
+#' @param cov_mu_sigma Covariance of (mu, sigma), default 0
+#'
+#' @return standard error of the headcount
+#' @export
+#' @examples
+#' HC_se_LN(900, mean_y = 2000, sigma = 0.36, se_mean = 250, se_sigma = 0.01)
+#' 0.01767621 # 1.76%
+HC_se_LN <- function(pov_line, mean_y, sigma, se_mean, se_sigma, cov_mu_sigma = 0) {
 
-  #LN_loc = mu_LN(mean_y, LN_sigma)
-
-  # compute z value
-  z <- (log(p / mean_y) / LN_sigma) + LN_sigma / 2
-
-  # normal pdf
+  z <- (log(pov_line / mean_y) / sigma) + sigma/2
   phi_z <- dnorm(z)
 
-  # derivatives
-  dH_dmean    <- phi_z * (-1 / (mean_y * LN_sigma))
-  dH_dsigma <- phi_z * ( -log(p / mean_y) / (LN_sigma^2) + 0.5 )
-  #dH_dmu <- phi_z * (-1 / (mean_y * LN_sigma)) * exp(LN_sigma^2/2 + LN_loc)
+  dH_dmean  <- phi_z * (-1 / (mean_y * sigma))
+  dH_dsigma <- phi_z * (-log(pov_line / mean_y) / sigma^2 + 0.5)
 
-  # delta-method variance
-  var_H <- (dH_dmean^2)    * se_Average^2 +
+  var_H <- (dH_dmean^2)  * se_mean^2 +
     (dH_dsigma^2) * se_sigma^2 +
-    2 * dH_dmean * dH_dsigma * cov_mu_sigma
+    2*dH_dmean*dH_dsigma*cov_mu_sigma
 
-  # var_H <- (dH_dmu^2)    * se_mu^2 +
-  #   (dH_dsigma^2) * se_sigma^2 +
-  #   2 * dH_dmean * dH_dsigma * cov_mu_sigma
-  sqrt(var_H)  # return SE
+  sqrt(var_H)
 }
--- R/NewPareto.R
+++ R/NewPareto.R
@@ -1,6 +1,15 @@
 #install.packages("hypergeo")
 library(hypergeo)
 library(nloptr)
+# packages
+if (!requireNamespace("numDeriv", quietly = TRUE)) {
+  install.packages("numDeriv")
+}
+if (!requireNamespace("dplyr", quietly = TRUE)) {
+  install.packages("dplyr")
+}
+library(numDeriv)
+library(dplyr)
 
 ## alpha is shape
 ## beta is scale
@@ -161,8 +170,8 @@ se_scale_NP <- function(mean_y, Gini, se_mean, se_Gini, shape=NA, se_shape = NA,
   return(se_scale)
 }
 
-## Function for the Lorenz Curve for the country -------------
-Lorenz_NP_fromY = function(y, shape, scale) { ## the income group y, shape and scale of the NPareto, mean income
+## Lorenz Curve for the country -------------
+CDF_NP_integral = function(y, shape, scale) { ## the income group y, shape and scale of the NPareto, mean income
 
   ## vector for the intergrals
 
@@ -175,7 +184,9 @@ Lorenz_NP_fromY = function(y, shape, scale) { ## the income group y, shape and s
   return(Lorenz)
 }
 
-Lorenz_NP <- function(p = seq(0.1, 1, by =0.1), shape){
+Lorenz_NP_Exact <- function(p = seq(0.1, 1, by =0.1), shape){
+
+  if (shape <= 0) stop("shape must be positive")
 
   doubleInt <- function(p,shape){
     f <- function(t,shape){((1+t)/(1-t))^(1/shape)}
@@ -205,11 +216,62 @@ Lorenz_NP <- function(p = seq(0.1, 1, by =0.1), shape){
   ## Gini integral
   denominator = Re(shape * hypergeo(1,-1/shape,2-1/shape,-1) / (shape-1) )
 
-  L = sapply(p, FUN = function(p) integrationTrap(func=doubleInt,lower=0, upper = p, shape=shape))
+  L_0 = sapply(p, FUN = function(p) integrationTrap(func=doubleInt,lower=0, upper = p, shape=shape))
 
-  return(L/denominator)
+  L = L_0*100/denominator
+  L[length(L)] = 100- sum(L[-length(L)])
+  return(cumsum(L))
   }
 
+
+# 1) Fast Lorenz (vectorized grid + interpolation)
+#    Uses a non-uniform grid with clustering near 1 to capture integrand behaviour.
+#    Notes / tuning
+#   - n_grid: increase for more accuracy (default 3000). If you need more speed, reduce.
+#   - cluster_power: greater -> more grid density near 1; adjust 2..5 as needed.
+#   - Hessian: numeric Hessian inversion provides an approximate covariance; for more
+#     accurate SEs you can bootstrap (resample grouped residuals) – more costly.
+Lorenz_NP <- function(p = seq(0.1, 1, by = 0.1),
+                           shape,
+                           n_grid = 3000,
+                           cluster_power = 3,
+                           eps = 1e-12) {
+  if (shape <= 0) stop("shape must be positive")
+  # create non-uniform grid on [0, 1) clustering near 1
+  u <- seq(0, 1, length.out = n_grid)
+  t_grid <- 1 - (1 - u)^cluster_power
+
+  # ensure last grid point slightly less than 1 to avoid blow-ups
+  t_grid[length(t_grid)] <- 1 - 1e-10
+
+  # inner integrand = ((1+t)/(1-t))^(1/shape)
+  inner_f <- function(t) {
+    ((1 + t) / (1 - t))^(1 / shape)
+  }
+
+  fvals <- inner_f(t_grid)
+
+  # cumulative integral by trapezoid: cumulative integral from 0 to t_i
+  # compute trapezoid increments
+  dx <- diff(t_grid)
+  trap_incr <- 0.5 * dx * (fvals[-1] + fvals[-length(fvals)])
+  cumint <- c(0, cumsum(trap_incr))  # same length as t_grid
+
+  ## denominator is cumulative integral at p = 1 (but that's limited from <1)
+  denom <- cumint[length(cumint)]
+  # denom <- Re(shape * hypergeo::hypergeo(1,-1/shape,2-1/shape,-1) / (shape-1) ) ## exact
+
+  if (!is.finite(denom) || denom <= 0) stop("denominator non-finite; check shape")
+
+  # for requested p, simple interpolation of cumint vs t_grid
+  # ensure p within [0, 1-eps]
+  p_req <- p
+  p_req[p_req >= 1] <- 1 - 1e-10
+  p_req[p_req < 0] <- 0
+  Lvals <- approx(x = t_grid, y = cumint, xout = p_req, rule = 2)$y / denom
+
+  return(Lvals)
+}
 ## Quantile function to build Lorenz curve, returns the percentile ------------
 Percentile_NP = function(shape, q) {
 
@@ -230,15 +292,17 @@ pdf_NP = function(shape,scale,p) {
 }
 
 ## CDF of NewPareto ----------------
-CDF_NP = function(shape,scale,q) {
-  #return(ifelse(q>= scale, (q^shape - scale^shape) / (q^shape + scale^shape),0))
-  return(ifelse(q>= scale, 1 -2 / ( 1+(q/scale)^shape),0))
+CDF_NP = function(q, shape,scale) {
+  q <- as.numeric(q)
+  out <- ifelse(q < scale, 0, 1 - 2 / (1 + (q / scale)^shape))
+  return(out)
 }
 
 ## Quantile Function from percentiles ------------------
-Quantile_NP = function(shape,scale,p) {
-  #return(ifelse(p<1, scale * ((-p-1)/(p-1))^(1/shape),100000000))
-  scale * ((2/(1-p) - 1)^(1/shape))
+Quantile_NP <- function(p, shape, scale) {
+  # p in (0,1). Solve 1 - 2/(1+(q/scale)^shape) = p => q = scale * ((2/(1-p)-1)^(1/shape))
+  if (any(p >= 1 | p <= 0)) stop("p must be in (0,1)")
+  scale * ((2 / (1 - p) - 1)^(1 / shape))
 }
 
 ## Radom Generated NP ----------------------
@@ -246,35 +310,7 @@ Quantile_NP = function(shape,scale,p) {
 random_NP <- function (u,a,b){(((2*(b^a))/u)-(b^a))^(1/a)}
 
 ## Gini from NP distribution ------------------
-Gini_NP <- function(shape){
-
-  # doubleInt <- function(p,shape){
-  #   f <- function(t,shape){((1+t)/(1-t))^(1/shape)}
-  #   fvalue <- integrate(f,lower=0,upper=p,shape=shape)$value
-  #   return(fvalue)
-  # }
-  #
-  # integrationTrap <- function (func, lower, upper, shape){
-  #   x = seq(lower,upper,by=0.001)  ## divide the x-axis into small intervals
-  #   n = length(x)
-  #   x <- as.matrix(x,n,1)
-  #   Y <- matrix(0,n,1)
-  #   Y <- as.matrix(Y,n,1)
-  #   tra <- matrix(0,n,1)
-  #
-  #   for(i in 1:n)
-  #   {
-  #     Y[i] <- func(x[i],shape)  ## the integral over evey portion/interval
-  #   }
-  #
-  #   tra[1] <- 0
-  #   for (i in 2:n) {
-  #     tra[i] <-((0.5)*(x[i]-x[i-1])*(Y[i]+Y[i-1]))  }
-  #   return(sum(tra))
-  # }
-  #
-  # ## Gini integral
-  # integ = integrationTrap(func=doubleInt,lower=0, upper = 1, shape=shape)/Re(shape * hypergeo(1,-1/shape,2-1/shape,-1) / (shape-1) )
+Gini_NP_Exact <- function(shape){
 
   integ = Lorenz_NP(p=1, shape= shape)
   gini <- 1- 2*integ
@@ -283,9 +319,49 @@ Gini_NP <- function(shape){
   return(gini)
 }
 
+# 2) Gini from Lorenz (numeric integration)
+
+Gini_NP <- function(shape, n_grid = 3000) {
+  # compute Lorenz on grid and integrate
+  p_grid <- seq(0, 1, length.out = n_grid)
+  L_grid <- Lorenz_NP(p_grid, shape = shape, n_grid = n_grid)
+  # integrate L(p) via trapezoid
+  dp <- diff(p_grid)[1]
+  integral_L <- sum(0.5 * (L_grid[-1] + L_grid[-length(L_grid)]) * dp)
+  G <- 1 - 2 * integral_L
+  return(G)
+}
+
 ## Mean from NP distribution -----------------
-mean_NP <- function(shape, scale){
-  2*shape *scale *Re(hypergeo(2,2-((1+shape)/shape),3-((1+shape)/shape),-1))/(shape - 1)
+Mean_NP_Exact <- function(shape, scale){
+  if (shape <= 1) {
+    return(Inf) # mean diverges for shape <= 1
+  }
+
+  2*shape *scale *Re(hypergeo::hypergeo(2,2-((1+shape)/shape),3-((1+shape)/shape),-1))/(shape - 1)
+}
+
+# 3) Mean via survival integral (numeric)
+#    E[X] = scale * (1 + 2 * ∫_0^1 t^(shape-2) / (1 + t^shape) dt)
+#    derived by substitution u = 1/x. Works for shape > 1.
+Mean_NP <- function(shape, scale, reltol = 1e-8) {
+  if (shape <= 1) {
+    return(Inf) # mean diverges for shape <= 1
+  }
+  integrand <- function(t) {
+    t^(shape - 2) / (1 + t^shape)
+  }
+  val <- tryCatch({
+    stats::integrate(integrand, lower = 0, upper = 1, rel.tol = reltol)$value
+  }, error = function(e) {
+    # fallback to a robust trapezoid integration on log-space if integrate fails
+    grid <- seq(0, 1, length.out = 2000)
+    grid[1] <- 1e-12
+    v <- integrand(grid)
+    sum(0.5 * (v[-1] + v[-length(v)]) * diff(grid))
+  })
+  mean_val <- scale * (1 + 2 * val)
+  return(mean_val)
 }
 
 ## MLE estimate -----------------------------------
@@ -331,7 +407,7 @@ functionMLE_NP <- function(z){                 ## Code from paper for maximum li
   return(results)
 }
 
-### Method 1 of estimating micro data ####
+## Method 1 of estimating micro data ####
 # estimated_params <- functionMLE(fyy2)
 #
 # min_value <- min(fyy2)
@@ -352,7 +428,7 @@ functionMLE_NP <- function(z){                 ## Code from paper for maximum li
 # }
 
 
-### Method 2 of estimating micro data ####
+## Method 2 of estimating micro data ####
 #
 # new_values1 <- random_NP(runif(fyy2), estimated_params$shapeEstimate, estimated_params$scaleEstimate)
 #
@@ -365,9 +441,7 @@ functionMLE_NP <- function(z){                 ## Code from paper for maximum li
 # Requires: nloptr (for your existing shape_NP / scale_NP), stats
 
 
-# ---------------------------
-# Bootstrap function (recommended if you have raw income vector)
-# ---------------------------
+## Bootstrap function (recommended if you have raw income vector) ---------------
 # incomes: numeric vector of person-level incomes (or unit-level)
 # R: number of bootstrap resamples (e.g., 1000)
 # parallel: "no" or use multicore? (not implemented here)
@@ -451,7 +525,7 @@ se_newpareto_boot <- function(incomes, R = 1000, seed = 1234, shape_init = NULL)
   )
 }
 
-### Headcount SE ------------------
+## Headcount SE ------------------
 HC_se_NP <- function(x, shape, scale, se_shape, se_scale, cov_shape_scale = 0) {
 
   # compute u = (p/scale)^shape
@@ -469,3 +543,255 @@ HC_se_NP <- function(x, shape, scale, se_shape, se_scale, cov_shape_scale = 0) {
   sqrt(var_H)  # return standard error
 }
 
+
+## fitgroup.np fitgroup-like wrapper + SEs/CIs  -------------------------------
+
+# a) EWMD-like objective (fit to grouped Lorenz points)
+#    We'll match the observed grouped Lorenz (pc.inc = vector of p: .1 ... 1)
+#    Optionally include Gini and mean in the objective via weights.
+objective_np <- function(par, L_obs,
+                         pc.inc = NULL, gini.e = NULL,
+                         mean_weight = 1, gini_weight = 1, lorenz_weight = 10,
+                         n_grid = 3000) {
+  shape <- par[1]
+  scale <- par[2]
+
+  p_obs = seq(0.1, 1, by = (1/(length(L_obs))))  ##L_obs is cumulative Lorenz points
+
+  # invalid penalties
+  if (shape <= 1 || scale <= 0) return(1e12)
+
+  # theoretical Lorenz at observed p
+  L_th <- Lorenz_NP(p_obs, shape = shape, n_grid = n_grid)
+
+  lorenz_err <- sum((L_th - L_obs)^2)
+
+  mean_err <- 0
+  if (!is.null(pc.inc)) {
+    mean_th <- Mean_NP(shape, scale)
+    mean_err <- (mean_th - pc.inc)^2
+  }
+
+  gini_err <- 0
+  if (!is.null(gini.e)) {
+    gini_th <- Gini_NP(shape)
+    gini_err <- (gini_th - gini.e)^2
+  }
+
+  # weighted sum of squared errors
+  loss <- lorenz_weight * lorenz_err + mean_weight * mean_err + gini_weight * gini_err
+  return(loss)
+}
+
+
+# b) fitgroup-like wrapper: fitgroup.np()
+#    Returns a list with ewmd.estimation ("Coef.", "se") and gini.estimation
+# y <- income_shares_nonCum (Lorenz_NP(shape= 3))
+# pc.inc <- Mean_NP(shape=3, scale= 1000)
+# gini.e <- Gini_NP(shape = 3)
+# fit_out <- fitgroup.np(y, pc.inc, gini.e)
+# print(fit_out)
+# fit_out$ewmd.estimation
+# Coefs = fit_out$ewmd.estimation["Coef.",]
+# ses =  fit_out$ewmd.estimation["se",]
+# conf_bound( Coefs, ses )
+fitgroup.np <- function(y,    # observed Non cumulative income shares at y (same length)
+                               pc.inc = NULL,  # observed mean (optional, can be used in obj)
+                               gini.e = NULL,  # observed gini (optional)
+                               gini = TRUE,
+                               N = NULL,
+                               se.ewmd = TRUE,
+                               se.scale = TRUE,
+                               nrep = 20,
+                               control = list() ) {
+
+
+  # sanitize inputs
+  L_obs = as.numeric(cumsum(y))
+  #if (length(y) != length(L_obs)) stop("y and L_obs must have same length")
+  #p_obs <- as.numeric(seq(0.1, 1, 1))   # vector of cumulative population shares (p, e.g., 0.1..1)
+
+  # initial guesses:
+  # - scale: use pc.inc/2 if mean provided, otherwise median quantile heuristic
+  start_scale <- if (!is.null(pc.inc) && is.finite(pc.inc) && pc.inc > 0) {
+    pc.inc / 2
+  } else {
+    1
+  }
+  start_shape <- 2
+
+  # optimization
+  opt <- tryCatch({
+    optim(par = c(start_shape, start_scale),
+          fn = objective_np,
+          L_obs = L_obs,
+          pc.inc = pc.inc,
+          gini.e = gini.e,
+          method = "L-BFGS-B",
+          lower = c(1 + 1e-6, 1e-8),
+          upper = c(500, 1e12),
+          control = modifyList(list(maxit = 1000), control)
+    )
+  }, error = function(e) {
+    list(par = c(NA, NA), value = Inf, converged = FALSE, message = e$message)
+  })
+
+  if (!isTRUE(opt$convergence == 0)) {
+    warning("fitgroup.np: optimization may not have converged: ", opt$convergence)
+  }
+
+  est <- opt$par
+  names(est) <- c("shape", "scale")
+
+  # compute standard errors using numeric Hessian on the objective function
+  se <- c(NA, NA)
+  if (se.ewmd && is.finite(opt$value) && all(is.finite(est))) {
+    # numeric Hessian of objective at optimum
+    hess <- tryCatch({
+      numDeriv::hessian(func = function(p) objective_np(p, L_obs = L_obs,
+                                                        pc.inc = pc.inc, gini.e = gini.e),
+                        x = est)
+    }, error = function(e) NULL)
+    ## Ensure symmetric hessian
+    for(k in 1:nrow(hess)){
+      for( l in 1:ncol(hess)) {
+           if(l != k) hess[l,k] = hess[k,l]
+    }}
+
+    if (!is.null(hess) && all(is.finite(hess))) {
+      # covariance approx = 2 * (H^{-1}) * s2 ? For least-squares-like objective, treat Hessian as 2 * J'J
+      # We'll use cov = solve(hess), then se = sqrt(diag(cov))
+      # But ensure hess invertible
+      inv_h <- tryCatch(solve(hess), error = function(e) NULL)
+      if(is.null(inv_h)) inv_h <- tryCatch(MASS::ginv(hess), error = function(e) NULL)
+      if (!is.null(inv_h)) {
+        se_val <- sqrt(pmax(0, diag(inv_h)))
+        # For scaling we don't have residual variance estimate; we present these "raw" se estimates
+        se <- se_val
+        names(se) <- c("shape", "scale")
+      }
+    }
+  }
+
+  # compute gini and mean estimates for the fitted params
+  gini_est <- tryCatch(Gini_NP(shape = est[1]), error = function(e) NA)
+  mean_est <- tryCatch(Mean_NP(shape = est[1], scale = est[2]), error = function(e) NA)
+
+  # produce results similar to fitgroup.* structures
+  out <- list(
+    ewmd.estimation = rbind("Coef." = est, "se" = se),
+    gini.estimation = c(mean = as.numeric(mean_est), gini = gini_est),
+    objective_value = opt$value,
+    converged = (opt$convergence == 0),
+    optim_output = opt
+  )
+
+  return(out)
+}
+
+
+## fitgroup.np_tidy and return tidy output with CIs ---------------------------------------------------------------
+# y <- income_shares_nonCum (Lorenz_NP(shape= 3))
+# pc.inc <- Mean_NP(shape=3, scale= 1000)
+# gini.e <- Gini_NP(shape = 3)
+# fit_out <- fitgroup.np_tidy("CountryX", y, pc.inc, gini.e)
+# print(fit_out)
+# attr(fit_out, "fit_object")$ewmd.estimation
+fitgroup.np_tidy <- function(country_name,
+                                y,
+                               pc.inc = NULL, gini.e = NULL, N = NULL,
+                               se.ewmd = TRUE, control = list()) {
+
+  fit <- fitgroup.np(y = L_vec,
+                            pc.inc = pc.inc, gini.e = gini.e, N = N,
+                            se.ewmd = se.ewmd, control = control)
+
+  coefs <- as.numeric(fit$ewmd.estimation["Coef.", ])
+  names(coefs) <- colnames(fit$ewmd.estimation)
+  ses <- as.numeric(fit$ewmd.estimation["se", ])
+
+  ##
+  ci_from_fit <- function(coefs, ses, level = 0.95) {
+      z <- qnorm((1 + level) / 2)
+
+      data.frame(
+        param = names(coefs),
+        est = coefs,
+        se = ses,
+        lower = coefs - z * ses,
+        upper = coefs + z * ses,
+        row.names = NULL
+      )
+    }
+
+  ci_tab <- ci_from_fit(coefs, ses )
+
+  tib <- tibble::tibble(
+    Country = country_name,
+    shape = coefs["shape"],
+    scale = coefs["scale"],
+    mean_est = fit$gini.estimation["mean"],
+    gini_est = fit$gini.estimation["gini"]
+  )
+
+  tib_ci <- ci_tab %>%
+    tidyr::pivot_wider(names_from = param,
+                       values_from = c(est, se, lower, upper),
+                       names_sep = "_")
+
+  # merge and return
+  out <- dplyr::bind_cols(tib, tib_ci)
+  attr(out, "fit_object") <- fit
+  return(out)
+}
+
+
+
+## fit NP----------------
+
+#' Fit the New Pareto (NP) Income Distribution
+#'
+#' @param y Numeric vector of non-cumulative income shares.
+#' @param pc.inc Observed mean income.
+#' @param gini.e Observed Gini coefficient.
+#' @param N Optional sample size (reserved for future SE estimation).
+#' @param se.ewmd Logical; placeholder for compatibility.
+#' @param se.scale Logical; placeholder for compatibility.
+#' @param nrep Integer; placeholder for bootstrapping future SEs.
+#' @param control List of optimizer controls (passed to \code{optim}).
+#'
+#' @return A list returned by \code{optim}.
+#'
+#' @details
+#' The function minimizes \code{loss_NP} over parameters \code{a} and \code{b}
+#' using L-BFGS-B with bounds ensuring valid NP parameters.
+#'
+#' @examples
+#' \dontrun{
+#' fit <- fit_np(y = deciles,
+#'               pc.inc = 20000,
+#'               gini.e = 0.32)
+#' fit$par
+#' }
+fit_np <- function(y, pc.inc, gini.e, N = NULL,
+                   se.ewmd = TRUE, se.scale = TRUE,
+                   nrep = 1000,
+                   control = list(maxit = 500))
+{
+  p_dec <- seq(0.1, 0.9, length.out = length(y) + 1)
+
+  out <- optim(
+    par    = c(a = 2, b = pc.inc / 2),
+    fn     = loss_NP,
+    pc.inc = pc.inc,
+    gini.e = gini.e,
+    y      = y,
+    p_dec  = p_dec,
+    method = "L-BFGS-B",
+    lower  = c(1.01, 1e-6),
+    upper  = c(50, 1e6),
+    control = control
+  )
+
+  out
+}
