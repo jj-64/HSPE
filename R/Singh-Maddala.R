2731bde6d253165745d5898120604743493a2de3
Jinane Jouni <jinane.jouni@hotmail.com>
2025-11-19 14:03:42 +0200

    added Lorenz curve

--- R/Singh-Maddala.R
+++ R/Singh-Maddala.R
@@ -1,21 +1,29 @@
 CDF_SM <- function(y, a, b, q)
 { VGAM::psinmad(q=y, scale = b , shape1.a = a, shape3.q = q) }
 
+Lorenz_SM <- function(p = seq(0.1,1,by =0.1), a, b, q) {
+  # Lorenz of Singh-Maddala
+  term <- (1 - p)^(1 - 1/a)
+  num  <- pbeta(1 - p, q - 1/a, 1 + 1/a)
+  den  <- beta(q, 1 + 1/a)
+  L <- 1 - term * num / den
+  return(L)
+}
 
-HC_se_SM <- function(pov_line, a, b, q, se_a, se_b, se_q) {
-  # pov_line: poverty line(s)
+HC_se_SM <- function(y, a, b, q, se_a, se_b, se_q) {
+  # y: poverty line(s)
   # a, b, q: Singh-Maddala parameters
   # vcov: 3x3 variance–covariance matrix from fitgroup.sm
 
-  pov_line <- as.numeric(pov_line)
+  y <- as.numeric(y)
 
   # components
-  v <- (pov_line / b)^a
+  v <- (y / b)^a
   u <- 1 + v
   Fval <- 1 - u^(-q)
 
   # partial derivatives
-  dF_da <-  q * u^(-q - 1) * v * log(pov_line / b)
+  dF_da <-  q * u^(-q - 1) * v * log(y / b)
   dF_db <- -q * u^(-q - 1) * (a / b) * v
   dF_dq <-  u^(-q) * log(u)
 
