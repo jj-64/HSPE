0b2fb436454c2ed0d72e850ad2143a7f109bf3c3
Jinane Jouni <jinane.jouni@hotmail.com>
2025-11-20 12:57:13 +0200

    Initial commit

---
+++ R/summarize_micro_data.R
@@ -0,0 +1,17 @@
+
+  # ---- 1. Load microdata  ----
+  summarize_micro_data <-function ( indices = NA,
+                       variable = "dhi",
+                       raw = FALSE,
+                       country_filter = NULL,
+                       update = FALSE) {
+
+    data_list <- read_data(indices = indices,
+                         variable = variable,
+                         raw = raw,
+                         country_filter = country_filter)
+
+  # ---- 2. Save clean microdata ----
+    if(update) save(micro_data_hic, file = "DATA/DataProcessed/micro_data_hic.rda")
+
+}
