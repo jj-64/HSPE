c12cedea45f04d7d43f615ea4a12be155fe1c5b7
Jinane Jouni <jinane.jouni@hotmail.com>
2025-11-27 11:26:09 +0200

    hypothetical file

---
+++ R/SumData.R
@@ -0,0 +1,16 @@
+#' Summary Dataset
+#'
+#' A dataset included in the HSPE package.
+#'
+#' @format A data frame with X rows and Y columns:
+#' \describe{
+#'   \item{var1}{Description of var1}
+#'   \item{var2}{Description of var2}
+#' }
+#'
+#' @usage data("SumData")
+#'
+#' @examples
+#' data(SumData)
+#' head(SumData)
+"SumData"
