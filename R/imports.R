7bd05718c4c870a46964b627e28b266fe8161fad
Jinane Jouni <jinane.jouni@hotmail.com>
2025-12-24 15:31:25 +0200

    initial commit

---
+++ R/imports.R
@@ -0,0 +1,14 @@
+#' @importFrom stats pnorm pbeta
+#' Box.test IQR aggregate coef cor cov lm median
+#'   na.omit nlminb optim pchisq predict pt qchisq qnorm quantile
+#'   rbeta rcauchy rexp rnorm runif rweibull sd var mad wilcox.test
+#'
+#' @importFrom utils head tail install.packages setTxtProgressBar txtProgressBar
+#'
+#' @importFrom magrittr %>%
+#'
+#' @importFrom dplyr arrange count filter mutate summarise all_of one_of desc
+#' @importFrom tidyr pivot_wider
+#' @importFrom numDeriv grad
+#' @importFrom GB2group fitgroup.ln fitgroup.f fitgroup.da fitgroup.b2 fitgroup.gb2 fitgroup.np fitgroup.sm
+NULL
