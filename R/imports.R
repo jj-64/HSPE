5721c2a2feaac89c2ce45a1ce92cb6b1f47a0234
Jinane Jouni <jinane.jouni@hotmail.com>
2025-12-24 16:14:17 +0200

    added import

--- R/imports.R
+++ R/imports.R
@@ -1,14 +1,29 @@
-#' @importFrom stats pnorm pbeta
-#' Box.test IQR aggregate coef cor cov lm median
-#'   na.omit nlminb optim pchisq predict pt qchisq qnorm quantile
-#'   rbeta rcauchy rexp rnorm runif rweibull sd var mad wilcox.test
+#' @importFrom stats pnorm dnorm qnorm pbeta Re integrate ks.test
 #'
 #' @importFrom utils head tail install.packages setTxtProgressBar txtProgressBar
 #'
 #' @importFrom magrittr %>%
 #'
-#' @importFrom dplyr arrange count filter mutate summarise all_of one_of desc
-#' @importFrom tidyr pivot_wider
-#' @importFrom numDeriv grad
-#' @importFrom GB2group fitgroup.ln fitgroup.f fitgroup.da fitgroup.b2 fitgroup.gb2 fitgroup.np fitgroup.sm
+#' @importFrom dplyr bind_cols bind_rows left_join select slice ungroup
+#' @importFrom tidyr pivot_wider pivot_longer
+#' @importFrom numDeriv grad hessian
+#' @importFrom GB2group fitgroup.ln fitgroup.f fitgroup.da fitgroup.b2
+#' fitgroup.gb2 fitgroup.np fitgroup.sm
+#' @importFrom VGAM psinmad ddagum pdagum qdagum dfisk pfisk qfisk dpareto
+#' @importFrom hypergeo hypergeo
+#' @importFrom nloptr nloptr
+#' @importFrom tibble tibble
+#' @importFrom ineq Gini ineq
+#' @importFrom giniVarCI igini
+#' @importFrom MASS ginv
+#' @importFrom extraDistr dbetapr pbetapr qbetapr
+#' @importFrom purrr map_dfr map2_dfr
+#' @importFrom fitdistrplus fitdist
+#' @importFrom writexl write_xlsx
+#' @importFrom rcartocolor carto_pal
+#' @importFrom colorspace lighten
+#' @importFrom scales percent
+#' @importFrom haven read_dta
+#' @importFrom
+#' @importFrom
 NULL
